e9ac899882d18f8c5715f07ec7a36fd3162a07b7
Taylor, Erica <Taylor.Erica@Principal.com>
2021-03-09 08:29:54 -0600

    Refactored FizzBuzz to remove local variables and insert calc for divisible by 3 or 5 within the if statements

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,22 +1,16 @@
 
 public class FizzBuzz {
     public String execute(Integer number) {
-        String result = "";
-        result = String.valueOf(number);
-        int divisibleByThree = number % 3;
-        int divisibleByFive = number % 5;
-        if (divisibleByThree == 0 && divisibleByFive == 0){
-            result = "FizzBuzz";
-        }
-        else if (divisibleByThree == 0) {
-            result = "Fizz";
-        }
-        else if (divisibleByFive == 0){
-            result = "Buzz";
+
+        if (number % 3 == 0 && number % 5 == 0){
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0){
+            return "Buzz";
         }
 
-        return result;
-
+        return String.valueOf(number);
 
     }
 }
