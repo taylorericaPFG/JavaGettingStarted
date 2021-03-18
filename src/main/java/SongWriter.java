512649ca861e9b16a22673b2f17e2a2625ab8c4f
Taylor, Erica <Taylor.Erica@Principal.com>
2021-03-18 09:54:15 -0500

    Add SongWriter class and methods

--- src/main/java/SongWriter.java
+++ src/main/java/SongWriter.java
@@ -6,6 +6,43 @@ public class SongWriter {
     private String songName;
     private int numberOfSongs;
 
+    SongWriter (String writerName){
+        this.writerName = writerName;
+    }
 
+    public String getWriterName() {
+        return writerName;
+    }
+
+    public void setWriterName(String writerName) {
+        this.writerName = writerName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setNumberOfSongs(int numberOfSongs) {
+        this.numberOfSongs = numberOfSongs;
+    }
+
+    public int getNumberOfSongs() {
+        return numberOfSongs;
+    }
+
+    public SongWriter (String writerName, String songName) {
+        this(writerName);
+        this.songName = songName;
+    }
+
+    public SongWriter (String writerName, String songName, int numberOfSongs) {
+        this(writerName);
+        this.songName = songName;
+        this.numberOfSongs = numberOfSongs;
+    }
 
 }
